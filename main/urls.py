eb4987f1004ebcc62b0cae70a1ce0194a038c5ea
exyzhao <ethanxyzhao@gmail.com>
2021-11-07 19:01:08 -0500

    First commit

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.main_view, name='main_view'),
+    path('splash/', views.splash_view, name='splash_view')
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def main_view(request):
     return render(request, 'main.html' )
+
+def splash_view(request):
+    return render(request, 'splash.html' )
